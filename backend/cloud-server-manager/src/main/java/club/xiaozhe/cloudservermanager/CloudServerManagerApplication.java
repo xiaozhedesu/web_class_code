aa4efc804f2afd1a152b8c1ae2795021e61753ab
xiaozhedesu <xiaozhedesu0219@outlook.com>
2026-05-25 20:28:59 +0800

    add: init backend project.

---
+++ backend/cloud-server-manager/src/main/java/club/xiaozhe/cloudservermanager/CloudServerManagerApplication.java
@@ -0,0 +1,13 @@
+package club.xiaozhe.cloudservermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudServerManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudServerManagerApplication.class, args);
+    }
+
+}
---
+++ backend/cloud-server-manager/src/test/java/club/xiaozhe/cloudservermanager/CloudServerManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package club.xiaozhe.cloudservermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudServerManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
