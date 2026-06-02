d9821b7d9e38723f7951679df6f3e1962bc09e69
xiaozhedesu <xiaozhedesu0219@outlook.com>
2026-06-02 17:35:59 +0800

    update(backend): 去掉secret配置项

--- backend/cloud-server-manager/src/main/java/club/xiaozhe/cloudservermanager/util/JwtUtil.java
+++ backend/cloud-server-manager/src/main/java/club/xiaozhe/cloudservermanager/util/JwtUtil.java
@@ -1,13 +1,11 @@
 package club.xiaozhe.cloudservermanager.util;
 
 import io.jsonwebtoken.*;
-import io.jsonwebtoken.security.Keys;
 import io.jsonwebtoken.security.SignatureException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
-import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.util.Map;
 import java.util.function.Function;
@@ -15,13 +13,10 @@ import java.util.function.Function;
 @Component
 public class JwtUtil {
 
-    private final SecretKey key;
+    // 采用自动生成的方案，是因为它作为练手项目，甚至没有配置redis，无需考虑保留用户登录态的任务，省掉更方便配置项目
+    private final SecretKey key = Jwts.SIG.HS512.key().build();
     @Value("${jwt.expiration}")
     private Long expiration;
-
-    public JwtUtil(@Value("${jwt.secret}") String secret) {
-        this.key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
-    }
 
     /**
      * 生成 token，携带用户名和角色
