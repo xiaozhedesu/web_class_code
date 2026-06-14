671b8045ec914640b3e62048b8b16591537124ac
xiaozhedesu <xiaozhedesu0219@outlook.com>
2026-06-14 23:12:28 +0800

    update(backend): 将requestMatchers的api提取成常量

--- backend/cloud-server-manager/src/main/java/club/xiaozhe/cloudservermanager/config/SecurityConfig.java
+++ backend/cloud-server-manager/src/main/java/club/xiaozhe/cloudservermanager/config/SecurityConfig.java
@@ -43,28 +43,30 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) {
         ObjectMapper mapper = new ObjectMapper();
 
-        http
-                .csrf(CsrfConfigurer::disable)
+        final String[] OPEN_PATH = {"/api/auth/**", "/error"};
+        final String[] ADMIN_PATH = {"/api/admin/**"};
+        final String[] USER_PATH = {"/api/user/**", "/api/servers"};
+
+        http.csrf(CsrfConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                // 自定义 401 未认证响应
                 .exceptionHandling(ex -> ex
+                        // 自定义 401 未认证响应
                         .authenticationEntryPoint((request, response, authException) -> {
                             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
                             response.setContentType("application/json;charset=UTF-8");
                             response.getWriter().write(mapper.writeValueAsString(ApiResponse.error(401, "请先登录")));
                         })
                         // 自定义 403 权限不足响应
-                        .accessDeniedHandler((request, response, accessDeniedException) -> {
+                        .accessDeniedHandler(((request, response, accessDeniedException) -> {
                             response.setStatus(HttpServletResponse.SC_FORBIDDEN);
                             response.setContentType("application/json;charset=UTF-8");
                             response.getWriter().write(mapper.writeValueAsString(ApiResponse.error(403, "权限不足")));
-                        })
+                        }))
                 )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**", "/error").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api/servers").authenticated()
-                        .requestMatchers("/api/admin/**").hasRole(User.Role.ADMIN.name())
-                        .requestMatchers("/api/user/**").authenticated()
+                        .requestMatchers(OPEN_PATH).permitAll()
+                        .requestMatchers(ADMIN_PATH).hasAuthority(User.Role.ADMIN.name())
+                        .requestMatchers(USER_PATH).authenticated()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
