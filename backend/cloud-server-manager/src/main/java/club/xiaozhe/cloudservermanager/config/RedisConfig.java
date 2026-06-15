9577aca92e34e742e0d096707c5ee287ed10a018
xiaozhedesu <xiaozhedesu0219@outlook.com>
2026-06-15 17:31:34 +0800

    add(backend): 添加Redis配置类

---
+++ backend/cloud-server-manager/src/main/java/club/xiaozhe/cloudservermanager/config/RedisConfig.java
@@ -0,0 +1,23 @@
+package club.xiaozhe.cloudservermanager.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.redis.host:localhost}")
+    private String redisHost;
+
+    @Value("${spring.redis.port:6379}")
+    private int redisPort;
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, redisPort);
+        return new LettuceConnectionFactory(config);
+    }
+}
