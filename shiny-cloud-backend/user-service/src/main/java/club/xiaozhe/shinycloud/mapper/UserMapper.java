b03ab60d59cd9396a5af5dd0302c7e5516edeb36
xiaozhedesu <xiaozhedesu0219@outlook.com>
2026-09-19 16:20:55 +0800

    feat(user-service): 添加 UserMapper.selectByUsername

--- shiny-cloud-backend/user-service/src/main/java/club/xiaozhe/shinycloud/mapper/UserMapper.java
+++ shiny-cloud-backend/user-service/src/main/java/club/xiaozhe/shinycloud/mapper/UserMapper.java
@@ -2,6 +2,11 @@ package club.xiaozhe.shinycloud.mapper;
 
 import club.xiaozhe.shinycloud.entity.User;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 
 public interface UserMapper extends BaseMapper<User> {
+    default User selectByUsername(String username) {
+        return selectOne(Wrappers.<User>lambdaQuery()
+                .eq(User::getUsername, username));
+    }
 }
